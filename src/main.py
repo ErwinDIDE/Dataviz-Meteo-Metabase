dee903f96fadb4b03a07b621c3eff9724111750a
Erwin DIDE <didymedide0808@gmail.com>
2026-06-04 00:58:01 -0700

    Mise a jour du fichier /src/main.py

--- src/main.py
+++ src/main.py
@@ -7,23 +7,20 @@ engine = create_engine(DATABASE_URL)
 
 def nettoyage_et_chargement():
     
-    file_path = "./data/raw/Q_01_avant-1949_RR-T-Vent.csv"
-    print(f"📥 Lecture directe du fichier CSV : {file_path}")
+    file_path_raw = "./data/raw/Q_01_avant-1949_RR-T-Vent.csv"
+    print(f"📥 Lecture directe du fichier CSV : {file_path_raw}")
     
     # Lecture du fichier
-    df = pd.read_csv(file_path, sep=';', low_memory=False)
+    df = pd.read_csv(file_path_raw, sep=';', low_memory=False)
     df.columns = df.columns.str.strip()
     
-    # Suppression des colonnes vides (TN, TX...)
-    df = df.dropna(how='all', axis=1)
-    
-    # Normalisation des colonnes
+    # Normalisation des colonnes de base
     if 'AAAAMMJJ' in df.columns:
         df['date'] = pd.to_datetime(df['AAAAMMJJ'].astype(str), format='%Y%m%d')
     if 'NUM_POSTE' in df.columns:
         df['code_departement'] = df['NUM_POSTE'].astype(str).str.zfill(8).str[:2]
 
-    # Renommer les colonnes restantes pour Metabase
+    # Renommage Metabase
     rename_dict = {
         'NUM_POSTE': 'id_station',
         'NOM_USUEL': 'nom_station',
@@ -35,10 +32,19 @@ def nettoyage_et_chargement():
     }
     df = df.rename(columns={k: v for k, v in rename_dict.items() if k in df.columns})
 
+    # Sélection des colonnes
+    colonnes_utiles = [
+        'id_station', 'nom_station', 'longitude', 'latitude', 
+        'altitude_metres', 'precipitations_mm', 'qualite_donnee_rr', 
+        'date', 'code_departement'
+    ]
+    # Conservation des colonnes sélectionnés
+    df = df[[col for col in colonnes_utiles if col in df.columns]]
+
     # Sauvegarde dans /processed
-    file_path = "./data/processed/meteo_1949.csv"
-    df.to_csv(file_path, index=False)
-    print(f"💾 Fichier nettoyé sauvegardé : {file_path}")
+    file_path_processed = "./data/processed/meteo_1949.csv"
+    df.to_csv(file_path_processed, index=False)
+    print(f"💾 Fichier nettoyé sauvegardé : {file_path_processed}")
     
     # Injection dans PostgreSQL
     table_name = "meteo_1949"
